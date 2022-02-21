ee9ab7428e7695fb24a02102eacdedabb43c9490
mardonier <mardonier@gmail.com>
2022-02-21 12:13:47 -0300

    Utilizando variáveis no javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+let olderPersonName = prompt("Digite o nome da pessoa mais velha")
+let olderPersonAge = prompt("Digite o idade da pessoa mais velha")
+
+let youngerPersonName = prompt("Digite o nome da pessoa mais nova")
+let youngerPersonAge = prompt("Digite o idade da pessoa mais nova")
+let ageDiuference = olderPersonAge - youngerPersonAge  
+
+alert("Pessoa mais velhar: " + olderPersonName + "\nIdade: " + olderPersonAge + 
+    "\n\nPessoa mais nova: " + youngerPersonName + "\nIdade: " + youngerPersonAge + 
+    "\n\nDiferença de idade: " + ageDiuference)
